50ca73f69884a40447b819e08638b5099b3299e1
sibhat <sibhat.temelso@gmail.com>
2019-06-04 08:26:30 -0700

    update .gitignore and auth0 variables

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  domain: "nickcannariato.auth0.com",
-  clientId: "tBTudXB9QQ9EfcHHlxBoQCa9OJ5uObC7",
+  domain: "training-bot-3.auth0.com",
+  clientId: "y5heetne1DGA1AwuMsE83xuEfUB28kp0",
   callbackUrl: process.env.REACT_APP_LOCAL || process.env.REACT_APP_PROD
   //=callbackUrl: "http://localhost:3000/callback"
 };
