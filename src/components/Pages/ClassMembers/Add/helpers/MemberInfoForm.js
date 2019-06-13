c004562703f20149bf7fd86190ff50ac76efa18e
sibhat <sibhat.temelso@gmail.com>
2019-06-12 22:52:23 -0700

    Update component props name to match its functionality. Getting accurate state info!

--- src/components/Pages/ClassMembers/Add/helpers/MemberInfoForm.js
+++ src/components/Pages/ClassMembers/Add/helpers/MemberInfoForm.js
@@ -13,7 +13,7 @@ function MemberInfoForm({ classes, state, updateMember }) {
         id="standard-name"
         label="First Name"
         className={classes.textFieldMain}
-        value={state.teamMember.first_name}
+        value={state.classMember.first_name}
         onChange={e => updateMember("first_name", e.target.value)}
         margin="normal"
         required
@@ -22,20 +22,12 @@ function MemberInfoForm({ classes, state, updateMember }) {
         id="standard-name"
         label="Last Name"
         className={classes.textFieldMain}
-        value={state.teamMember.last_name}
+        value={state.classMember.last_name}
         onChange={e => updateMember("last_name", e.target.value)}
         margin="normal"
         required
       />
-      <TextField
-        id="standard-name"
-        label="Job Description"
-        className={classes.textFieldMain}
-        value={state.teamMember.job_description}
-        onChange={e => updateMember("job_description", e.target.value)}
-        margin="normal"
-        required
-      />
+
       <NumberFormat
         format="+1 (###) ###-####"
         type="tel"
@@ -43,7 +35,7 @@ function MemberInfoForm({ classes, state, updateMember }) {
         label="Phone Number"
         customInput={TextField}
         className={classes.textFieldMain}
-        value={state.teamMember.phone_number}
+        value={state.classMember.phone_number}
         onChange={e => updateMember("phone_number", e.target.value)}
         margin="normal"
         required
@@ -53,7 +45,7 @@ function MemberInfoForm({ classes, state, updateMember }) {
         label="Email"
         type="email"
         className={classes.textFieldMain}
-        value={state.teamMember.email}
+        value={state.classMember.email}
         onChange={e => updateMember("email", e.target.value)}
         margin="normal"
       />
