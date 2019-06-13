d81eb5958de05d1e8332a6ad73f75dcaaf05333d
sibhat <sibhat.temelso@gmail.com>
2019-06-12 23:13:12 -0700

    Update component props name to match its functionality. Getting accurate state info!

--- src/components/Pages/ClassMembers/Edit/Edit.js
+++ src/components/Pages/ClassMembers/Edit/Edit.js
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import axios from "axios";
 
-import EditTeamMember from "../Add/";
+import EditClassMember from "../Add/";
 import NotificationsCard from "components/Pages/Notifications/Card/";
 import TeamMemberNotifications from "components/Pages/Notifications/Card/TeamMember";
 import Grid from "@material-ui/core/Grid";
 
 import {
-  editTeamMember,
+  editClassMember,
   getTrainingSeries,
-  getTeamMemberByID
+  getClassMemberByID
 } from "store/actions";
 
 import { EditWrapper } from "./styles.js";
@@ -19,41 +19,42 @@ function Edit(props) {
   const {
     match,
     user_id,
-    getTeamMemberByID: getTMFromProps,
+    getClassMemberByID: getCMFromProps,
     getTrainingSeries: getTSFromProps
   } = props;
 
   useEffect(() => {
-    getTMFromProps(match.params.id);
+    getCMFromProps(match.params.id);
     getTSFromProps(user_id);
-  }, [getTMFromProps, getTSFromProps, match, user_id]);
+  }, [getCMFromProps, getTSFromProps, match, user_id]);
   const [secretMsg, setSecretMsg] = useState("");
 
   const sendMsgMeow = () => {
     // This was a demo function only, never intended to be used in production.
     // But if you want to test Slack notifications, just uncheck display:none in your dev tools
-    const { first_name, slack_uuid } = props.teamMember;
+    const { first_name, slack_uuid } = props.classMember;
     const notification = {
       first_name,
       subject: "Slack Test",
       body: secretMsg,
       slack_uuid,
-      team_member_id: props.teamMember.id
+      class_member_id: props.classMember.id
     };
     const url = `${process.env.REACT_APP_API}/api/slack/sendMessageNow`;
     axios.post(url, { notification });
     setSecretMsg("");
   };
-  const buttonText = props.teamMember.slack_uuid
-    ? "Send Msg Meow"
-    : "No Slack ID";
+  // const buttonText = props.classMember.slack_uuid
+  //   ? "Send Msg Meow"
+  //   : "No Slack ID";
+  const buttonText = "No Slack ID";
   return (
     <EditWrapper>
       <div style={{ display: "none" }}>
         {/* We used this to demo Slack messages*/}
         <input value={secretMsg} onChange={e => setSecretMsg(e.target.value)} />
         <button
-          disabled={!props.teamMember.slack_uuid}
+          // disabled={!props.classMember.slack_uuid}  // TODO understand the idea here
           onClick={() => sendMsgMeow()}
         >
           {buttonText}
@@ -61,7 +62,7 @@ function Edit(props) {
       </div>
 
       <Grid item xs={12} lg={5}>
-        <EditTeamMember user_id={user_id} teamMember={props.teamMember} />
+        <EditClassMember user_id={user_id} classMember={props.classMember} />
       </Grid>
       <Grid item xs={12} lg={5}>
         <NotificationsCard
@@ -77,10 +78,10 @@ function Edit(props) {
 
 const mapStateToProps = state => ({
   trainingSeries: state.trainingSeriesReducer.trainingSeries,
-  teamMember: state.teamMembersReducer.teamMember
+  classMember: state.classMembersReducer.classMember
 });
 
 export default connect(
   mapStateToProps,
-  { getTeamMemberByID, editTeamMember, getTrainingSeries }
+  { getClassMemberByID, editClassMember, getTrainingSeries }
 )(Edit);
