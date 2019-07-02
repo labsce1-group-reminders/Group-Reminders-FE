fe88a5da1ce6f6ed90f0a53e939275475aab0498
sibhat <sibhat.temelso@gmail.com>
2019-07-01 17:28:51 -0700

    clean up console.logs, and reformat code.

--- src/components/Pages/Classes/List/Tab/Tab.js
+++ src/components/Pages/Classes/List/Tab/Tab.js
@@ -1,55 +1,53 @@
 // main page for displaying list of all training series
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
+import React, {useEffect} from "react";
+import {connect} from "react-redux";
 
 import history from "history.js";
-import { getClasses } from "store/actions";
+import {getClasses} from "store/actions";
 
-import { Classes } from "./styles.js";
-import { Typography } from "@material-ui/core/";
+import {Classes} from "./styles.js";
+import {Typography} from "@material-ui/core/";
 
-function Tab({ id, getFiltered, getClasses, classes, dispatch }) {
-  useEffect(() => {
-    console.log("use effect", id)
-    getClasses();
-    
-  }, [getClasses, id]);
+function Tab({id, getFiltered, getClasses, classes}) {
+    useEffect(() => {
+        getClasses();
 
-  return (
-    
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      {getFiltered(classes).map(classObj => {
-        const className = classObj.title
-        return (
-          <Classes key={classObj.id}>
-            <div
-              style={{ cursor: "pointer" }}
-              onClick={e => {
-                history.push(`/home/class/${classObj.id}`);
-              }}
-            >
-              <Typography variant="subtitle1">
-                {className} 
-              </Typography>
-              <hr />
-          </div>
-          </Classes>
+    }, [getClasses, id]);
 
-        );
-      })}
-    </div>
-  );
+    return (
+
+        <div style={{display: "flex", flexWrap: "wrap"}}>
+            {getFiltered(classes).map(classObj => {
+                const className = classObj.title;
+                return (
+                    <Classes key={classObj.id}>
+                        <div
+                            style={{cursor: "pointer"}}
+                            onClick={()=> {
+                                history.push(`/home/class/${classObj.id}`);
+                            }}
+                        >
+                            <Typography variant="subtitle1">
+                                {className}
+                            </Typography>
+                            <hr/>
+                        </div>
+                    </Classes>
+
+                );
+            })}
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
-  console.log("map state to props", state);
-  return ({
-  classes: state.classesReducer.classes
-});
-}
+    return ({
+        classes: state.classesReducer.classes
+    });
+};
 
 export default connect(
-  mapStateToProps,
-  { getClasses }
+    mapStateToProps,
+    {getClasses}
 )(Tab);
 //(withStyles(styles)(Tab));
